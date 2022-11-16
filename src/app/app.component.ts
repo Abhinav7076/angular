adcf82d8de3115828a5bb22ed98e15419d13008a
Abhinav7076 <abhinav.pratapsingh555@gmail.com>
2022-11-16 10:56:26 +0530

    angular basic proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name = "aps";
   title = 'my-dream-app';
 }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    // selector: 'app-server',
+    // selector: '[app-server]', //selecting by attribute
+    selector: '.app-server', //selecting by class
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+}
