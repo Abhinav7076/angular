da80b6c877b3b89c344a5f800189f820eae42974
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-11-16 20:30:25 +0530

    taking input

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,26 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    // selector: '[app-server]', //selecting by attribute
-    // selector: '.app-server', //selecting by class
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
-    serverId: number = 10;
-    serverStatus: string = 'server stalled';
-    allowedServer = false;
+    input=''
 
-    constructor(){
-        setTimeout(() => {
-            this.allowedServer = true;
-        }, 2000);
-    }
-
-    getServerStatus(){
-        return this.serverStatus
-    }
-
-    changeStatus(){
-        this.serverStatus = 'server running'
+    getInput(event: Event){
+        this.input = (<HTMLInputElement>event.target).value
     }
 }
