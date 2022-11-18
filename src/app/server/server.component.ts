345486014592ab117075506e32132de38edc3d02
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-11-18 10:32:14 +0530

    ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,25 +2,25 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online{
+            color: white
+        }
+    `]
 })
 export class ServerComponent{
     input=''
     serverCreated=false
     on=true
     port1Status=''
-    port2Status=''
 
     constructor(){
         this.port1Status = Math.random() > 0.5 ? 'online' : 'offline'
-        this.port2Status = Math.random() > 0.5 ? 'online' : 'offline'
     }
 
     getColor(){
         return this.port1Status === 'online' ? 'green' : 'red'
-    }
-    getColor2(){
-        return this.port2Status === 'online' ? 'green' : 'red'
     }
     
 }
