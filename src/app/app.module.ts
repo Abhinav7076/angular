d801aa287fa85a745c5246aeec23858f72f96569
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-11-23 11:09:17 +0530

    custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { StudentProfileComponent } from './home/student-profile/student-profile.component';
-import { TakeInputComponent } from './home/take-input/take-input.component';
-import { StudentDetailComponent } from './home/student-detail/student-detail.component';
+import { basicHighlight } from './custom-directive/basic_highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    StudentProfileComponent,
-    TakeInputComponent,
-    StudentDetailComponent
+    basicHighlight
   ],
   imports: [
     FormsModule,
