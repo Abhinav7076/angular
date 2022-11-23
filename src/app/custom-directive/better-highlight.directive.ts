4ba3c5851ae16f9e073db57a8913f128ab7a0416
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-11-23 11:56:08 +0530

    Binding to property of our own directive

--- src/app/custom-directive/better-highlight.directive.ts
+++ src/app/custom-directive/better-highlight.directive.ts
@@ -1,23 +1,25 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, INJECTOR, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
+  @Input() defaultColor = 'transparent'
+  @Input() highlightedColor = 'yellow'
   @HostBinding('style.backgroundColor') backgroundColor : string = 'transparent'
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
+    this.backgroundColor = this.defaultColor
   }
 
   @HostListener('mouseenter') mouseEnter(eventData: Event){
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
-    this.backgroundColor = 'yellow'
+    this.backgroundColor = this.highlightedColor
   }
   @HostListener('mouseleave') mouseLeave(eventData: Event){
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
-    this.backgroundColor = 'transparent'
+    this.backgroundColor = this.defaultColor
   }
 }
