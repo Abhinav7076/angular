9afb7d9acaa3ce6446284740da015b631823d3f0
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-12-07 11:20:06 +0530

    using observable given by interval

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { dataService } from './data.service';
 import { profile } from './profile.model';
-
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  profiles: profile[] = []
-  selected_profile: profile
-
-  @Input() feature: string
-
-  onStudentAdded(studentData: {name: string, rollno: string, department: string}){
-    this.profiles.push({name: studentData.name, rollno: studentData.rollno, department: studentData.department})
-  }
-
-  recieve(student_data: profile){
-    this.selected_profile = student_data
-  }
   
   constructor(private dataService: dataService) { }
 
   ngOnInit() {
-    this.profiles = this.dataService.profiles
-    console.log(this.dataService.profiles)
+    interval(1000).subscribe(count => {
+      console.log(count)
+    })
   }
 
 }
