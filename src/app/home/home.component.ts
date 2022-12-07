b9e0957c44f8aed68ffe97a2207c9ca481d4aa9d
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-12-07 12:17:30 +0530

    unsubsribing an observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { dataService } from './data.service';
 import { profile } from './profile.model';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   
+  subscription: Subscription
+  count = 0
+
   constructor(private dataService: dataService) { }
 
   ngOnInit() {
-    interval(1000).subscribe(count => {
+    this.subscription = interval(1000).subscribe(count => {
+      this.count=count
       console.log(count)
     })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 
 }
