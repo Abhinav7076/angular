aa7229a58c95a1a01a1393c7799644c4968fb881
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-12-08 10:26:35 +0530

    timer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,27 +13,45 @@ export class HomeComponent implements OnInit, OnDestroy {
   count = 0
 
   constructor(private dataService: dataService) { }
+  timeLeft: number = 3;
+  interval;
+
+startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        clearInterval(this.interval);
+      }
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
 
   ngOnInit() {
-    const customIntervalObservable = Observable.create(observer=>{
-      let count=0
-      setInterval(()=>{
-        observer.next(count)
-        if(count==2)
-          observer.complete()
-        if(count>3)
-          observer.error(new Error('Count greater than 3'))
-        count++
-      }, 1000)
-    })
-    customIntervalObservable.subscribe(data=>{
-      console.log(data)
-    }, error =>{
-      console.log(error)
-      alert(error)
-    }, ()=>{
-      console.log('Completed')
-    })
+    // const customIntervalObservable = Observable.create(observer=>{
+    //   let count=0
+    //   setInterval(()=>{
+    //     observer.next(count)
+    //     // this.count = count
+    //     if(count==0)
+    //       observer.complete()
+    //     if(count>3)
+    //       observer.error(new Error('Count greater than 3'))
+    //     count++
+    //   }, 1000)
+    // })
+    // customIntervalObservable.subscribe(data=>{
+    //   console.log(data)
+    // }, error =>{
+    //   console.log(error)
+    //   alert(error)
+    // }, ()=>{
+    //   console.log('Completed')
+    // })
+    this.startTimer()
   }
 
   ngOnDestroy(): void {
