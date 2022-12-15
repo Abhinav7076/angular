ad0d9a9b6710d6fc925f685ee8b0d0659a2fefc4
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-12-15 11:05:22 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-// import { dataService } from './home/data.service';
-// import { profile } from './home/quiz.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  // profiles: profile[] = []
+export class AppComponent implements OnInit {
+  loadedPosts = [];
 
-  // constructor(private dataService: dataService) {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-    // this.profiles = this.dataService.profiles
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { RouterModule, Routes } from '@angular/router';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OptionsComponent } from './home/options/options.component';
-import { ScoreComponent } from './home/score/score.component';
-import { dataService } from './home/data.service';
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'score', component: ScoreComponent},
-  { path: 'not-found', component: PagenotfoundComponent},
-  { path: '**', redirectTo: '/not-found'}
-]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    OptionsComponent,
-    HeaderComponent,
-    PagenotfoundComponent,
-    ScoreComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    BrowserAnimationsModule
-  ],
-  providers: [dataService],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
